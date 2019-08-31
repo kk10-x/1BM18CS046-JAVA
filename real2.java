f9d35481ba9f583e85f17b65f684b301b69c0081
kk10-x <53857387+kk10-x@users.noreply.github.com>
2019-08-31 20:34:39 +0530

    Update real2.java

--- real2.java
+++ real2.java
@@ -3,15 +3,17 @@ public class real2
 {
     public static void main(String args[])
     {
-        double x,y,dist;
+        double x,y,dist,a,b;
         Scanner in=new Scanner(System.in);
         System.out.println("Enter the value of x,y");
         x=in.nextInt();
         y=in.nextInt();
         point ob=new point();
         point ob1=new point(x,y);
-        dist=ob1.dist();
-        
+        SYstem.out.println("Enter the value of the other point")
+        a=in.nextInt();   
+        b=in.nextInt();
+        dist=ob1.dist(a,b);
         System.out.println("the distance between the point is:"+dist);
         
     }
