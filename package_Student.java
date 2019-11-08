a841356bb0d6d53662e3c1dd6af7daa6b4e40f9b
kk10-x <53857387+kk10-x@users.noreply.github.com>
2019-11-08 23:27:20 +0530

    Create package_Student.java

---
+++ package_Student.java
@@ -0,0 +1,22 @@
+package Cie;
+import java.util.*;
+public class Student{
+	public String usn, name;
+	public int sem;
+	Scanner sc = new Scanner(System.in);
+	public Student(){
+		usn = "";
+		name = "";
+		sem = 0;
+	}
+	/*
+	void input(){
+		System.out.print("Enter USN of the Student : ");
+		usn = sc.next();
+		System.out.print("Enter Name of the Student : ");
+		name = sc.next();
+		System.out.print("Enter Sem of the Student : ");
+		sem = sc.nextInt();
+	}
+	*/
+}
